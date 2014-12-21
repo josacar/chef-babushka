3df487a924270f3296928ce2107b43e0d0dddf36
Jose Luis Salas <josacar@gmail.com>
2014-12-21 19:32:16 +0000

    Use README as long description

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,11 @@ maintainer       'Jose Luis Salas'
 maintainer_email 'josacar@gmail.com'
 license          'MIT'
 description      'Installs/Configures babushka'
-long_description 'Installs/Configures babushka'
-version          '0.1.0'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
+
+supports 'debian'
+supports 'ubuntu'
+supports 'centos'
 
 depends 'git'
